4db996a4742753d7bdf45449d05e5a4acf6bda99
Ron Shteinberg <sh.ron.me@gmail.com>
2025-11-05 08:44:58 +0200

    remove interval

--- src/utils/jobQueue.ts
+++ src/utils/jobQueue.ts
@@ -69,8 +69,3 @@ export const enqueueLoan = (
 };
 
 export const queueLength = () => queue.length;
-
-// Start a periodic worker to pick up any stray jobs (in case setImmediate misses)
-setInterval(() => {
-  if (!running && queue.length > 0) setImmediate(worker);
-}, 5000);
